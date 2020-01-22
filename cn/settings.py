46deabba4b7ce46937ee441000ab8cbe698939b6
koma00 <koma196604@gmail.com>
2020-01-22 21:14:16 +0200

    Create application account

--- cn/settings.py
+++ cn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kzykhqk#wrw)fqp-s!td@)ykq7$z7k1rh@_q06tbym#6_^d&h3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.10']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
