24ce14e1e656133a0f2b35a11774b0609730f732
koma00 <koma199604@gmail.com>
2020-02-02 15:13:22 +0200

    Added sex in profile

--- account/models.py
+++ account/models.py
@@ -3,8 +3,15 @@ from django.conf import settings
 
 
 class Profile(models.Model):
+    SEX_CHOICES = (
+        ('none', 'none'),
+        ('male', 'male'),
+        ('female', 'female')
+    )
+
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
+    sex = models.CharField(max_length=20, choices=SEX_CHOICES, default='none')
     photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
 
     def __str__(self):
