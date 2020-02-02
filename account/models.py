f78770af5e6b5ab33bcc03b1a8cb692d8809455f
koma00 <koma199604@gmail.com>
2020-02-02 15:37:09 +0200

    Added views user foto on main page

--- account/models.py
+++ account/models.py
@@ -12,7 +12,7 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
     sex = models.CharField(max_length=20, choices=SEX_CHOICES, default='none')
-    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    photo = models.ImageField(upload_to='media/users/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
